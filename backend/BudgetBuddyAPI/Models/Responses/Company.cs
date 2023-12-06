da245a6ec9c59f25b12d39228d9a8e10425fecc7
Lucas Augusto <lucas.fernandes0522@gmail.com>
2023-12-05 22:35:45 -0300

    Adicionado campo ID na Company

--- backend/BudgetBuddyAPI/Models/Responses/Company.cs
+++ backend/BudgetBuddyAPI/Models/Responses/Company.cs
@@ -2,6 +2,7 @@
 {
     public class Company
     {
+        public int Id { get; set; }
         public string Name { get; set; } =  string.Empty;
         public string? Trading { get; set; }
         public string Document { get; set; } =  string.Empty;
