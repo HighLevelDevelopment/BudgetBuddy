3eef50c68b3b225b90f436eea21edaf66d8c58e8
peedroca <phm1080@gmail.com>
2023-12-15 22:18:48 -0300

    Added cloud files

--- backend/BudgetBuddyAPI/Program.cs
+++ backend/BudgetBuddyAPI/Program.cs
@@ -19,11 +19,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.ConfigureStatus();
