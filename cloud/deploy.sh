239f927563e5bd7de17cc43bdf4d10fb02c4c6e9
peedroca <phm1080@gmail.com>
2023-12-15 22:33:46 -0300

    Added deploy script

--- cloud/deploy.sh
+++ cloud/deploy.sh
@@ -5,5 +5,9 @@ cd repos/BudgetBuddy
 sudo git checkout devops
 sudo git pull
 
+sudo docker stop budgetbuddyapi
+sudo docker rm budgetbuddyapi
+sudo docker rmi budgetbuddyapi
+
 sudo docker build -t budgetbuddyapi -f dockerbuilds/webapi backend/BudgetBuddyAPI/
 sudo docker run -d -p 8080:80 --name budgetbuddyapi budgetbuddyapi
